911efc4d34834aec81351c1daceaf22ccb36ff7d
LeoDm123 <leo.meiners1@gmail.com>
2024-02-10 21:07:45 -0300

    cors config

--- app.js
+++ app.js
@@ -9,27 +9,6 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://leadspring.vercel.app/"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  next();
-});
-
 dbConnection();
 
 app.use("/auth", require("./src/routes/auth"));
