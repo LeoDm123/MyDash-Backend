859e0231bb166c8084fb6703448f939242701177
LeoDm123 <leo.meiners1@gmail.com>
2025-04-07 18:41:21 -0300

    funciones de recuperación de contraseña

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -12,32 +12,36 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const resetUrl = `${process.env.FRONTEND_URL}reset-password.html?token=${token}`;
-
-const sendEmail = async (email, token, type) => {
+const sendMail = async (email, token, type = "verify") => {
   let subject, html;
 
   if (type === "reset") {
+    const resetUrl = `${process.env.FRONTEND_URL}/reset-password.html?token=${token}`;
     subject = "Recuperación de contraseña de cuenta en TodoBeca.com";
-    html = `<h2>Recuperá tu contraseña</h2>
-           <p>Para cambiar tu contraseña, haz clic en el siguiente enlace:</p>
-           <a href="${resetUrl}">Restablecer contraseña</a>
-           <p>Este enlace expirará en 30 minutos.</p>`;
+    html = `
+      <h2>Recuperá tu contraseña</h2>
+      <p>Para cambiar tu contraseña, hacé clic en el siguiente enlace:</p>
+      <a href="${resetUrl}">Restablecer contraseña</a>
+      <p>Este enlace expirará en 30 minutos.</p>
+    `;
   } else {
+    const verifyUrl = `${process.env.FRONTEND_URL}/verify-email?token=${token}`;
     subject = "Verificación de email en TodoBeca.com";
-    html = `<h2>Verificá tu email</h2>
-           <p>Para completar tu registro, haz clic en el siguiente enlace:</p>
-           <a href="${process.env.FRONTEND_URL}/verify-email" onclick="sessionStorage.setItem('verificationToken', '${token}')">Verificar Email</a>`;
+    html = `
+      <h2>Verificá tu email</h2>
+      <p>Para completar tu registro, hacé clic en el siguiente enlace:</p>
+      <a href="${verifyUrl}">Verificar Email</a>
+    `;
   }
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: email,
-    subject: subject,
-    html: html,
+    subject,
+    html,
   };
 
   await transporter.sendMail(mailOptions);
 };
 
-module.exports = sendEmail;
+module.exports = sendMail;
