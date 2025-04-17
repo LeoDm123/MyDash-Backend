ac93a8df9cfd016c2ca74a8c37a96880320c14c5
LeoDm123 <leo.meiners1@gmail.com>
2025-04-17 14:36:22 -0300

    corrección app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { dbConnection } = require("./src/database/config");
-const app = express();
 const cors = require("cors");
 const path = require("path");
-
 require("dotenv").config();
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+const app = express();
 
+app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "public")));
 
 dbConnection();
 
@@ -17,6 +17,11 @@ app.use("/auth", require("./src/routes/auth"));
 app.use("/beca", require("./src/routes/beca"));
 app.use("/parametros", require("./src/routes/parametros"));
 
+const {
+  generarSitemapController,
+} = require("./src/controllers/auth.controllers");
+app.get("/sitemap.xml", generarSitemapController);
+
 app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+  console.log(`✅ Servidor corriendo en el puerto ${process.env.PORT}`);
 });
