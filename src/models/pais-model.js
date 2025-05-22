2f84de57cc6049541b607b0c0a43994491062db5
LeoDm123 <leo.meiners1@gmail.com>
2025-05-22 16:40:52 -0300

    condicion de unico a cada pais

--- src/models/pais-model.js
+++ src/models/pais-model.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const paisSchema = new Schema({
-  nombre: { type: String, required: true },
+  nombre: { type: String, required: true, unique: true },
   ficha_general_pais: {
     capital: { type: String, required: true },
     idiomas_oficiales: [{ type: String }],
